d1b6b979e9b4b46e1946e79ecf69f074ec9e7ce8
Boss <ruslangr@yahoo.com>
2021-02-13 15:09:35 +0200

    add Console add container

--- api/bin/app.php
+++ api/bin/app.php
@@ -4,14 +4,16 @@
 declare(strict_types=1);
 
 use App\Console\HelloCommand;
+use Psr\Container\ContainerInterface;
 use Symfony\Component\Console\Application;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-
+/** @var  ContainerInterface $container */
+$container = require __DIR__ . '/../config/container.php';
 
 $cli = new Application('Console');
+$cli->add($container->get(HelloCommand::class));
 
-$cli->add(new HelloCommand());
 
 $cli->run();
