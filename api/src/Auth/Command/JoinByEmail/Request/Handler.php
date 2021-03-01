d521b1462b2ad1bce715bc7442f7eb598e61df0a
Boss <ruslangr@yahoo.com>
2021-03-01 15:06:41 +0200

    JoinByEmail with simple params

--- api/src/Auth/Command/JoinByEmail/Request/Handler.php
+++ api/src/Auth/Command/JoinByEmail/Request/Handler.php
@@ -4,33 +4,61 @@
 namespace App\Auth\Command\JoinByEmail\Request;
 
 
+use DateTimeImmutable;
+use DomainException;
+use Ramsey\Uuid\Uuid;
 use function mb_strtolower;
 use function password_hash;
 use const PASSWORD_ARGON2I;
 
 class Handler
 {
+    /**
+     * @var UserRepository
+     */
+    private UserRepository $users;
+
+    public function __construct(UserRepository $users)
+    {
+        $this->users = $users;
+    }
     public function handler(Command $command): void
     {
-        $user = new User(
-            mb_strtolower($command->email),
-            password_hash($command->password, PASSWORD_ARGON2I)
+        $email = mb_strtolower($command->email);
 
+        if ($this->users->hasByEmail($email)){
+            throw new DomainException('User already exists');
+        }
+
+        $user = new User(
+            Uuid::uuid4()->toString(),
+            new DateTimeImmutable(),
+            $email,
+            password_hash($command->password, PASSWORD_ARGON2I),
+            Uuid::uuid4()->toString()
         );
+
+        $this->users->add($user);
     }
 }
 
 
 class User
 {
-    private ?int $id; // Id autoincrement in DB
+//    private ?int $id; // Id autoincrement in DB
+    private int $id;
+    private DateTimeImmutable $date;
     private string $email;
     private string $hash;
+    private string $token;
 
-    public function __construct(string $email, string $hash)
+    public function __construct(int $id, DateTimeImmutable $date , string $email, string $hash, string $token)
     {
+        $this->id = $id;
+        $this->$date = $date;
         $this->email = $email;
         $this->hash = $hash;
+        $this->token = $token;
     }
 
     /**
