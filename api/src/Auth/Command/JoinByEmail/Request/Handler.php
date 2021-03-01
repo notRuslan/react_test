df055e8fb065b3f83bea00ba6d183e69143da3b7
Boss <ruslangr@yahoo.com>
2021-03-01 20:51:52 +0200

    JoinByEmail with Entries

--- api/src/Auth/Command/JoinByEmail/Request/Handler.php
+++ api/src/Auth/Command/JoinByEmail/Request/Handler.php
@@ -1,71 +1,64 @@
 <?php
 
+declare(strict_types=1);
 
 namespace App\Auth\Command\JoinByEmail\Request;
 
-
+use App\Auth\Entity\User\Email;
+use App\Auth\Entity\User\Id;
+use App\Auth\Entity\User\User;
+use App\Auth\Entity\User\UserRepository;
+use App\Auth\Service\JoinConfirmationSender;
+use App\Auth\Service\PasswordHasher;
+use App\Auth\Service\Tokenizer;
+use App\Flusher;
 use DateTimeImmutable;
 use DomainException;
-use Ramsey\Uuid\Uuid;
-use function mb_strtolower;
-use function password_hash;
-use const PASSWORD_ARGON2I;
 
 class Handler
 {
-    /**
-     * @var UserRepository
-     */
     private UserRepository $users;
+    private PasswordHasher $hasher;
+    private Tokenizer $tokenizer;
+    private Flusher $flusher;
+    private JoinConfirmationSender $sender;
 
-    public function __construct(UserRepository $users)
-    {
+    public function __construct(
+        UserRepository $users,
+        PasswordHasher $hasher,
+        Tokenizer $tokenizer,
+        Flusher $flusher,
+        JoinConfirmationSender $sender
+    ) {
         $this->users = $users;
+        $this->hasher = $hasher;
+        $this->tokenizer = $tokenizer;
+        $this->flusher = $flusher;
+        $this->sender = $sender;
     }
-    public function handler(Command $command): void
-    {
-        $email = mb_strtolower($command->email);
 
-        if ($this->users->hasByEmail($email)){
-            throw new DomainException('User already exists');
+    public function handle(Command $command): void
+    {
+        $email = new Email($command->email);
+
+        if ($this->users->hasByEmail($email)) {
+            throw new DomainException('User already exists.');
         }
 
+        $date = new DateTimeImmutable();
+
         $user = new User(
-            Uuid::uuid4()->toString(),
-            new DateTimeImmutable(),
+            Id::generate(),
+            $date,
             $email,
-            password_hash($command->password, PASSWORD_ARGON2I),
-            Uuid::uuid4()->toString()
+            $this->hasher->hash($command->password),
+            $token = $this->tokenizer->generate($date)
         );
 
         $this->users->add($user);
-    }
-}
 
+        $this->flusher->flush();
 
-class User
-{
-//    private ?int $id; // Id autoincrement in DB
-    private int $id;
-    private DateTimeImmutable $date;
-    private string $email;
-    private string $hash;
-    private string $token;
-
-    public function __construct(int $id, DateTimeImmutable $date , string $email, string $hash, string $token)
-    {
-        $this->id = $id;
-        $this->$date = $date;
-        $this->email = $email;
-        $this->hash = $hash;
-        $this->token = $token;
-    }
-
-    /**
-     * @return int|null
-     */
-    public function getId(): ?int
-    {
-        return $this->id;
+        $this->sender->send($email, $token);
     }
 }
