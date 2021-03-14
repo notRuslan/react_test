566323b502eb20cbd29420cab3755ce67229ae00
Boss <ruslangr@yahoo.com>
2021-03-14 12:52:30 +0200

    add Controllers tests

--- api/config/common/frontend.php
+++ api/config/common/frontend.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Frontend\FrontendUrlGenerator;
+use Psr\Container\ContainerInterface;
 
 return [
     FrontendUrlGenerator::class => function (ContainerInterface $container): FrontendUrlGenerator {
