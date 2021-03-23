b517aea6076468f5edd4148665decac9517ed5bf
Boss <ruslangr@yahoo.com>
2021-03-23 15:47:46 +0200

    react 16 like lessons

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,5 +11,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-
-reportWebVitals();
+serviceWorker.unregister();
